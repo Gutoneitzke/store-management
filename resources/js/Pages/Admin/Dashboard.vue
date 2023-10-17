c7be7bc600ba2554f867285feca16276726110d0
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-10-16 22:48:19 -0300

    feat: new dashboard file with vue structure

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Dashboard file
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default {
+    components:{
+        Link
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
