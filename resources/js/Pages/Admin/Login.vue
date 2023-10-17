7dab8ca31accc36fc2ca13a36061397d523c8a1a
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-10-16 22:49:35 -0300

    feat: organize colors and login file

---
+++ resources/js/Pages/Admin/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Login file
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default {
+    components:{
+        Link
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
