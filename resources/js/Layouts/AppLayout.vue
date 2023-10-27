173a85e409aa4c50935b0caa4e4c069da7cd4f98
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-10-26 21:52:43 -0300

    feat: refactor select stores template

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -90,97 +90,38 @@ const logout = () => {
                                     Clientes
                                 </NavLink>
                             </div>
-
-                            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        </div>
+                        
+                        <div class="hidden sm:items-center space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <!-- Settings Dropdown -->
                             <div class="ml-3 relative">
-                                <!-- Teams Dropdown -->
-                                <Dropdown v-if="$page.props.jetstream.hasTeamFeatures" align="right" width="60">
+                                <Dropdown align="right" width="48">
                                     <template #trigger>
                                         <span class="inline-flex rounded-md">
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                                {{ $page.props.auth.user.current_team.name }}
+                                                {{ !$page.props.mySelectedStore ? 'Todas as lojas' : $page.props.mySelectedStore.name }}
 
                                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                                 </svg>
                                             </button>
                                         </span>
                                     </template>
 
                                     <template #content>
-                                        <div class="w-60">
-                                            Team Management
-                                            <!-- <div class="block px-4 py-2 text-xs text-gray-400">
-                                                Manage Team
-                                            </div> -->
-
-                                            <!-- Team Settings -->
-                                            <!-- <DropdownLink :href="route('teams.show', $page.props.auth.user.current_team)">
-                                                Team Settings
-                                            </DropdownLink> -->
-
-                                            <!-- <DropdownLink v-if="$page.props.jetstream.canCreateTeams" :href="route('teams.create')">
-                                                Create New Team
-                                            </DropdownLink> -->
-
-                                            <!-- Team Switcher -->
-                                            <template v-if="$page.props.auth.user.all_teams.length > 1">
-                                                <div class="border-t border-gray-200" />
-
-                                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                                    Switch Teams
-                                                </div>
-
-                                                <template v-for="team in $page.props.auth.user.all_teams" :key="team.id">
-                                                    <form @submit.prevent="switchToTeam(team)">
-                                                        <DropdownLink as="button">
-                                                            <div class="flex items-center">
-                                                                <svg v-if="team.id == $page.props.auth.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                                </svg>
-
-                                                                <div>{{ team.name }}</div>
-                                                            </div>
-                                                        </DropdownLink>
-                                                    </form>
-                                                </template>
-                                            </template>
+                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                            Escolha a Loja que você deseja gerenciar
                                         </div>
+
+                                        <DropdownLink 
+                                            v-for="s,i in $page.props.myStores" 
+                                            :key="i" 
+                                            :href="route('stores.selected',{id: s.id})" 
+                                        >
+                                            {{ s.name }}
+                                        </DropdownLink>
                                     </template>
                                 </Dropdown>
-                            </div>
-                            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                                <!-- Settings Dropdown -->
-                                <div class="ml-3 relative">
-                                    <Dropdown align="right" width="48">
-                                        <template #trigger>
-                                            <span class="inline-flex rounded-md">
-                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                                    {{ !$page.props.mySelectedStore ? 'Todas as lojas' : $page.props.mySelectedStore.name }}
-
-                                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                                    </svg>
-                                                </button>
-                                            </span>
-                                        </template>
-
-                                        <template #content>
-                                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                                Escolha a Loja que você deseja gerenciar
-                                            </div>
-
-                                            <DropdownLink 
-                                                v-for="s,i in $page.props.myStores" 
-                                                :key="i" 
-                                                :href="route('stores.selected',{id: s.id})" 
-                                            >
-                                                {{ s.name }}
-                                            </DropdownLink>
-                                        </template>
-                                    </Dropdown>
-                                </div>
-                            </div>
                             </div>
                         </div>
 
