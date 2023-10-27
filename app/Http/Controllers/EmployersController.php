de4401d3e6d68326be7988fcdf531ac1dad03684
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-10-27 20:22:18 -0300

    feat: show employers according stores

--- app/Http/Controllers/EmployersController.php
+++ app/Http/Controllers/EmployersController.php
@@ -2,17 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\UserStore;
+use App\Traits\HasSelectedStoreTrait;
+use App\Traits\MyStoresTrait;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class EmployersController extends Controller
 {
+    use HasSelectedStoreTrait;
+    use MyStoresTrait;
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return Inertia::render('StoreManagement/Employers');
+        $selectedStore = $this->getSelectedStore();
+
+        if($selectedStore){
+            $employers = User::where('type', 'EMPLOYEE')
+                            ->join('users_has_stores', 'users.id', '=', 'users_has_stores.users_id')
+                            ->where('users_has_stores.stores_id', $selectedStore['id'])
+                            ->where('users.id', '!=', auth()->id())
+                            ->get();
+        } else {
+            $employers = User::where('type', 'EMPLOYEE')
+                            ->join('users_has_stores', 'users.id', '=', 'users_has_stores.users_id')
+                            ->whereIn('users_has_stores.stores_id', $this->getMyStoresTrait())
+                            ->where('users.id', '!=', auth()->id())
+                            ->get();
+        }
+
+        return Inertia::render('StoreManagement/Employers/Employers',[
+            'employers' => $employers
+        ]);
     }
 
     /**
