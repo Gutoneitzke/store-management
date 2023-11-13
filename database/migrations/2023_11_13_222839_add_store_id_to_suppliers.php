24b7d6a176f78c248706f48e14be1bf5b7f2214c
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-11-13 19:38:59 -0300

    feat: added new field in suppliers table

---
+++ database/migrations/2023_11_13_222839_add_store_id_to_suppliers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->unsignedBigInteger('stores_id');
+
+            $table->foreign('stores_id')
+                ->references('id')
+                ->on('stores')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropForeign(['stores_id']);
+            $table->dropColumn('stores_id');
+        });
+    }
+};
