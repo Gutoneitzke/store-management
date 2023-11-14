fe24c9fad1a3876de0e3a76a0819668826660870
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-11-14 19:03:02 -0300

    feat: create product

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
+use App\Models\Store;
+use App\Models\Supplier;
+use App\Traits\GenerateRandomNumberTrait;
 use App\Traits\GetCountryStateCityTrait;
 use App\Traits\HasSelectedStoreTrait;
 use App\Traits\MyStoresTrait;
@@ -14,6 +18,7 @@ class StocksController extends Controller
     use GetCountryStateCityTrait;
     use MyStoresTrait;
     use HasSelectedStoreTrait;
+    use GenerateRandomNumberTrait;
 
     /**
      * Display a listing of the resource.
@@ -38,7 +43,17 @@ class StocksController extends Controller
      */
     public function create()
     {
-        //
+        $products = Product::whereIn('stores_id', $this->getMyStores())->get();
+        $categories = Category::whereIn('stores_id', $this->getMyStores())->get();
+        $suppliers = Supplier::whereIn('stores_id', $this->getMyStores())->get();
+        $myStores = Store::whereIn('id', $this->getMyStores())->get();
+
+        return Inertia::render('StoreManagement/Stocks/NewProduct',[
+            'products'   => $products,
+            'categories' => $categories,
+            'suppliers'  => $suppliers,
+            'myStores'   => $myStores
+        ]);
     }
 
     /**
@@ -46,7 +61,32 @@ class StocksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            if($request['newProduct']['status'] == '1' && $request['newProduct']['product_id'] == null){
+                $productData = [
+                    'name'        => $request['name'],
+                    'description' => $request['description'],
+                    'total_price' => $request['qty'] * $request['unity_price'],
+                    'qty_stock'   => $request['qty'],
+                    'stores_id'   => $request['stores_id'],
+                    'code'        => $this->generateRandomNumber()
+                ];
+    
+                $productId  = Product::create($productData);
+                
+            } else {
+                $productData = [];
+                $productId = $request['newProduct']['product_id'];
+            }
+
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Falha ao registrar o produto!');
+        }
+        // products -> ok
+        // product_entries -> --
+        // products_has_entries
+        // products_has_categories
+        // supplier_has_products
     }
 
     /**
