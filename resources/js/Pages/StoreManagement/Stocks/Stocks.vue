5a55413516e52b2f97e76702edf0030e8d5a490c
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-11-13 21:08:50 -0300

    refactor: rename NAME to NOME in tables

--- resources/js/Pages/StoreManagement/Stocks/Stocks.vue
+++ resources/js/Pages/StoreManagement/Stocks/Stocks.vue
@@ -32,7 +32,7 @@
                         <th 
                             class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                         >
-                            Name
+                            Nome
                         </th>
                         <th 
                             class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
