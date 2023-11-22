5855233755c3e1456206c2a5e1df7d34b47efa3f
Gutoneitzke <gutoneitzke12345@gmail.com>
2023-11-22 09:02:33 -0300

    feat: new doughnut chart

---
+++ resources/js/Components/DoughnutChart.vue
@@ -0,0 +1,32 @@
+<template>
+  <Doughnut
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { Doughnut } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+  name: 'DoughnutChart',
+  components: { Doughnut },
+  props: ['labels','datasets'],
+  data() {
+    return {
+      chartData: {
+        labels: this.labels,
+        datasets: this.datasets
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  }
+}
+</script>
